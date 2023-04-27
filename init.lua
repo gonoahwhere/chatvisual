b3b0dd97a4ec22caa05a69b23b72433b9ef78838
Noah <56979358+gonoahwhere@users.noreply.github.com>
2023-04-27 15:29:15 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,6 @@
+local modpath = minetest.get_modpath('chatvisual')
+local version = "1.0.0"
+
+dofile(modpath .. "/callback.lua") -- callback
+
+print('[MOD]'.. " Chat Visual has successfully loaded: [ version " .. version .. " ].")
